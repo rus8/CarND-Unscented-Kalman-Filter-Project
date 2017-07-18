960773fbce427346538574fa943989e6f45c7632
Ruslan <ruswaserty@yandex.ru>
2017-07-18 15:31:42 +0300

    angle normalization

--- src/tools.cpp
+++ src/tools.cpp
@@ -16,3 +16,9 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     * Calculate the RMSE here.
   */
 }
+
+double Tools::AngleNorm(double angle){
+  while(angle > M_PI) angle -= 2 * M_PI;
+  while(angle < -M_PI) angle += 2 * M_PI;
+  return angle;
+}
