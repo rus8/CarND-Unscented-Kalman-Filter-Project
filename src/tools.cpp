827c31cefd25be5e7c6b92964280c001f6718a3a
Ruslan <ruswaserty@yandex.ru>
2017-07-22 20:21:13 +0300

    Indentation, small fixes

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,14 +11,14 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    /**
+    TODO:
+      * Calculate the RMSE here.
+    */
 }
 
 double Tools::AngleNorm(double angle){
-  while(angle > M_PI) angle -= 2 * M_PI;
-  while(angle < -M_PI) angle += 2 * M_PI;
-  return angle;
+    while(angle > M_PI) angle -= 2 * M_PI;
+    while(angle < -M_PI) angle += 2 * M_PI;
+    return angle;
 }
