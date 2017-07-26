cc5ba6e08a19727e49083d57cd987768e680f51c
Ruslan <ruswaserty@yandex.ru>
2017-07-26 12:18:11 +0300

    radar update

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -175,14 +175,14 @@ void UKF::Prediction(double delta_t) {
     MatrixXd Xsig_aug = MatrixXd(n_aug_, n_sig_);
 
     Xsig_aug.col(0) = x_aug;
-    for (int i = 0; i < n_aug_; i++)
+    for (int i = 0; i < n_sig_; i++)
     {
         Xsig_aug.col(i+1) = x_aug + sqrt(lambda_+n_aug_) * A.col(i);
         Xsig_aug.col(i+1+n_x_) = x_aug - sqrt(lambda_+n_aug_) * A.col(i);
     }
 
     //predict sigma points
-    for (int i = 0; i< 2*n_aug_+1; i++)
+    for (int i = 0; i< n_sig_; i++)
     {
         //extract values for better readability
         double p_x = Xsig_aug(0,i);
@@ -224,24 +224,25 @@ void UKF::Prediction(double delta_t) {
         Xsig_pred_(2,i) = v_p;
         Xsig_pred_(3,i) = yaw_p;
         Xsig_pred_(4,i) = yawd_p;
+    }
 
-        //predicted state mean
-        x_.fill(0.0);
-        for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
-            x_ = x_ + weights_(i) * Xsig_pred_.col(i);
-        }
+    //predicted state mean
+    x_.fill(0.0);
+    for (int i = 0; i < n_sig_; i++) {  //iterate over sigma points
+        x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+    }
 
-        //predicted state covariance matrix
-        P_.fill(0.0);
-        for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+    //predicted state covariance matrix
+    P_.fill(0.0);
+    for (int i = 0; i < n_sig_; i++) {  //iterate over sigma points
 
-            // state difference
-            VectorXd x_diff = Xsig_pred_.col(i) - x_;
-            //angle normalization
-            x_diff(3) = Tools::AngleNorm(x_diff(3));
+        // state difference
+        VectorXd x_diff = Xsig_pred_.col(i) - x_;
+        //angle normalization
+        x_diff(3) = Tools::AngleNorm(x_diff(3));
 
-            P_ = P_ + weights_(i) * x_diff * x_diff.transpose();
-        }
+        P_ = P_ + weights_(i) * x_diff * x_diff.transpose();
+    }
 }
 
 /**
@@ -272,4 +273,73 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
     You'll also need to calculate the radar NIS.
     */
+
+    //size of measuremnt vector
+    int n_z = 3;
+
+    //matrix for transformed sigma points
+    MatrixXd Zsig = MatrixXd(n_z, n_sig_);
+
+    //transform sigma points into measurement space
+    for (int i = 0; i < n_sig_; i++) {  //2n+1 simga points
+        // extract values for better readibility
+        double p_x = Xsig_pred(0,i);
+        double p_y = Xsig_pred(1,i);
+        double v  = Xsig_pred(2,i);
+        double yaw = Xsig_pred(3,i);
+
+        double v1 = cos(yaw)*v;
+        double v2 = sin(yaw)*v;
+
+        // measurement model
+        Zsig(0,i) = sqrt(p_x*p_x + p_y*p_y);                        //r
+        Zsig(1,i) = atan2(p_y,p_x);                                 //phi
+        Zsig(2,i) = (p_x*v1 + p_y*v2 ) / sqrt(p_x*p_x + p_y*p_y);   //r_dot
+    }
+
+    //mean predicted measurement
+    VectorXd z_pred = VectorXd(n_z);
+    z_pred.fill(0.0);
+    for (int i=0; i < n_sig_; i++) {
+        z_pred = z_pred + weights_(i) * Zsig.col(i);
+    }
+
+    //measurement covariance matrix S
+    MatrixXd S = MatrixXd(n_z,n_z);
+    S.fill(0.0);
+    //matrix for cross correlation Tc
+    MatrixXd Tc = MatrixXd(n_x_, n_z);
+    Tc.fill(0.0);
+
+    for (int i = 0; i < n_sig_; i++) {
+        //residual
+        VectorXd z_diff = Zsig.col(i) - z_pred;
+        //angle normalization
+        z_diff(1) = Tools::AngleNorm(z_diff(1));
+
+        S = S + weights_(i) * z_diff * z_diff.transpose();
+
+        // state difference
+        VectorXd x_diff = Xsig_pred.col(i) - x_;
+        //angle normalization
+        x_diff(1) = Tools::AngleNorm(x_diff(1));
+
+        Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
+    }
+
+    //add measurement noise covariance matrix
+    S = S + R_radar_;
+
+    //Kalman gain K;
+    MatrixXd K = Tc * S.inverse();
+
+    //residual
+    VectorXd z_diff = meas_package.raw_measurements_ - z_pred;
+    //angle normalization
+    z_diff(1) = Tools::AngleNorm(z_diff(1));
+
+    //update state mean and covariance matrix
+    x_ = x_ + K * z_diff;
+    P_ = P_ - K*S*K.transpose();
+
 }
