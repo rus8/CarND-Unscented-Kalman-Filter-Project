12d7174d649f2cfbe7ec5d9ed8d17fe750c699d1
Ruslan <ruswaserty@yandex.ru>
2017-07-26 09:43:53 +0300

    UKF::Prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -1,6 +1,7 @@
 #include "ukf.h"
 #include "Eigen/Dense"
 #include <iostream>
+#include "tools.h"
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -36,7 +37,7 @@ UKF::UKF() {
     P_ = MatrixXd(n_x_, n_x_);
 
     // sigma points matrix
-    Xsig_pred_ = MatrixXd(n_aug_, n_sig_);
+    Xsig_pred_ = MatrixXd(n_x_, n_sig_);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
     std_a_ = 30;
@@ -71,6 +72,10 @@ UKF::UKF() {
                 0, 0, std_radrd_ * std_radrd_;
 
     weights_ = VectorXd(n_sig_);
+    weights_(0) = lambda_ / (lambda_ + n_aug_);
+    for (int i = 1; i < n_sig_; i++) {
+        weights_(i) = 0.5 / (n_aug_ + lambda_);
+    }
 
 
 }
@@ -120,12 +125,9 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 
         time_us_ = meas_package.timestamp_;
 
-        weights_(0) = lambda_ / (lambda_ + n_aug_);
-        for (int i = 1; i < n_sig_; i++) {
-            weights_(i) = 0.5 / (n_aug_ + lambda_);
-        }
-
         is_initialized_ = true;
+
+        return;
     }
 
     double dt;
@@ -155,6 +157,91 @@ void UKF::Prediction(double delta_t) {
     Complete this function! Estimate the object's location. Modify the state
     vector, x_. Predict sigma points, the state, and the state covariance matrix.
     */
+
+    //create augmented state
+    VectorXd x_aug = VectorXd(n_aug_);
+
+    //create P_aug for augmented state
+    MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+    P_aug.fill(0.0);
+    P_aug.topLeftCorner(5,5) = P_;
+    P_aug(5,5) = std_a_*std_a_;
+    P_aug(6,6) = std_yawdd_*std_yawdd_;
+
+    //calculate square root of P_aug
+    MatrixXd A = P_aug.llt().matrixL();
+
+    //set augmented sigma points
+    MatrixXd Xsig_aug = MatrixXd(n_aug_, n_sig_);
+
+    Xsig_aug.col(0) = x_aug;
+    for (int i = 0; i < n_aug_; i++)
+    {
+        Xsig_aug.col(i+1) = x_aug + sqrt(lambda_+n_aug_) * A.col(i);
+        Xsig_aug.col(i+1+n_x_) = x_aug - sqrt(lambda_+n_aug_) * A.col(i);
+    }
+
+    //predict sigma points
+    for (int i = 0; i< 2*n_aug_+1; i++)
+    {
+        //extract values for better readability
+        double p_x = Xsig_aug(0,i);
+        double p_y = Xsig_aug(1,i);
+        double v = Xsig_aug(2,i);
+        double yaw = Xsig_aug(3,i);
+        double yawd = Xsig_aug(4,i);
+        double nu_a = Xsig_aug(5,i);
+        double nu_yawdd = Xsig_aug(6,i);
+
+        //predicted state values
+        double px_p, py_p;
+
+        //avoid division by zero
+        if (fabs(yawd) > 0.001) {
+            px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+            py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+        }
+        else {
+            px_p = p_x + v*delta_t*cos(yaw);
+            py_p = p_y + v*delta_t*sin(yaw);
+        }
+
+        double v_p = v;
+        double yaw_p = yaw + yawd*delta_t;
+        double yawd_p = yawd;
+
+        //add noise
+        px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+        py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+        v_p = v_p + nu_a*delta_t;
+
+        yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+        yawd_p = yawd_p + nu_yawdd*delta_t;
+
+        //write predicted sigma point into right column
+        Xsig_pred_(0,i) = px_p;
+        Xsig_pred_(1,i) = py_p;
+        Xsig_pred_(2,i) = v_p;
+        Xsig_pred_(3,i) = yaw_p;
+        Xsig_pred_(4,i) = yawd_p;
+
+        //predicted state mean
+        x_.fill(0.0);
+        for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+            x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+        }
+
+        //predicted state covariance matrix
+        P_.fill(0.0);
+        for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+
+            // state difference
+            VectorXd x_diff = Xsig_pred_.col(i) - x_;
+            //angle normalization
+            x_diff(3) = Tools::AngleNorm(x_diff(3));
+
+            P_ = P_ + weights_(i) * x_diff * x_diff.transpose();
+        }
 }
 
 /**
